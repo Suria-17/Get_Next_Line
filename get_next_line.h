6d1776d68decf3ea39da12f12551e44a198cb9a6
Suria Narayanan Ramasamy <sramasam@u90z04s03.42kl.edu.my>
2025-07-20 13:48:21 +0800

    made adjustment according to Norm and included 42 header

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sramasam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 13:15:41 by sramasam          #+#    #+#             */
+/*   Updated: 2025/07/20 13:16:14 by sramasam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
-size_t ft_strlen(const char *s);
-char *ft_strchr(const char *s, int c);
-char *ft_strjoin(const char *s1, const char *s2);
-char *ft_strdup(const char *s);
-char *ft_substr(const char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 #endif
