04f8b08b9df1371ee4554674e58565eaa5e43308
Suria Narayanan Ramasamy <surianarayanan01@gmail.com>
2025-07-24 00:19:08 +0800

    Included read and hold & extract line function Included default buffer size in header

--- get_next_line.c
+++ get_next_line.c
@@ -11,3 +11,56 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char *read_and_hold(int fd, char *buffer)
+{
+    char *temp;
+    int bytes_read;
+
+    temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if(!temp)
+        return (NULL);
+    bytes_read = 1;
+    while (!ft_strchr(buffer, '\n') && bytes_read != 0)
+    {
+        bytes_read = read(fd, temp, BUFFER_SIZE);
+        if (bytes_read == -1)
+        {
+            free(temp);
+            return (NULL);
+        }
+        temp[bytes_read] = '\0';
+        buffer = ft_strjoin(buffer, temp);
+    }
+    free(temp);
+    return (buffer);
+}
+
+static char *extract_line(char *buffer)
+{
+    int len;
+    int i;
+    char *line;
+
+    len = 0;
+    if (!buffer[len])
+        return (NULL);
+    while (buffer[len] && buffer[len] != '\n')
+        len;
+    line = malloc(sizeof(char) * (len + 2));
+    if (!line)
+        return (NULL);
+    i = 0;
+    while (buffer[i] && buffer[i] != '\n')
+    {
+        line[i] = buffer[i];
+        i++;
+    }
+    if (buffer[i] == '\n')
+    {
+        line[i] = buffer[i];
+        i++;
+    }
+    line[i] = '\0';
+    return (line);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -16,6 +16,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+#endif
+
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(const char *s1, const char *s2);
