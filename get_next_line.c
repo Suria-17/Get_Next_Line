a9cc0622cc4f75f2b768190581bf66fe4ce0d19a
Suria Narayanan Ramasamy <sramasam@u90z06s01.42kl.edu.my>
2025-08-24 14:47:33 +0800

    Amended 1. read_data - declaration was done below instead of the beginning. 2. extract_line - declaration was done below instead of the beginning. 		- initialization of value was done in the declaration 		  line, separated it. 3. refresh_buffer - declaration was done below instead of the beginning. 		  - initialization of value was done in the declaration 		    line, separated it. 		  - added free(buffer) within new_buffer loop.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sramasam <sramasam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 13:40:25 by sramasam          #+#    #+#             */
-/*   Updated: 2025/08/22 21:39:22 by sramasam         ###   ########.fr       */
+/*   Updated: 2025/08/24 14:28:07 by sramasam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 char	*read_data(int fd, char *buffer)
 {
-	char *temp;
-	int bytes_read;
+	char	*temp;
+	char	*new_buffer;
+	int		bytes_read;
 
 	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!temp)
 		return (NULL);
-
 	bytes_read = read(fd, temp, BUFFER_SIZE);
 	if (bytes_read == -1)
 	{
@@ -33,22 +33,18 @@ char	*read_data(int fd, char *buffer)
 		return (buffer);
 	}
 	temp[bytes_read] = '\0';
-
-	char *new_buffer = ft_strjoin(buffer, temp);
+	new_buffer = ft_strjoin(buffer, temp);
 	free(buffer);
 	free(temp);
-
 	if (!new_buffer)
 		return (NULL);
-
 	return (new_buffer);
 }
 
-int has_newline(char *buffer)
+int	has_newline(char *buffer)
 {
 	if (!buffer)
 		return (0);
-
 	while (*buffer)
 	{
 		if (*buffer == '\n')
@@ -58,15 +54,14 @@ int has_newline(char *buffer)
 	return (0);
 }
 
-char *read_and_hold(int fd, char *buffer)
+char	*read_and_hold(int fd, char *buffer)
 {
-	char *new_buffer;
+	char	*new_buffer;
 
 	if (!buffer)
 		buffer = ft_strdup("");
 	if (!buffer)
 		return (NULL);
-
 	while (!has_newline(buffer))
 	{
 		new_buffer = read_data(fd, buffer);
@@ -78,24 +73,25 @@ char *read_and_hold(int fd, char *buffer)
 			return (NULL);
 		}
 		if (new_buffer == buffer)
-			break;
-
+			break ;
 		buffer = new_buffer;
 	}
 	return (buffer);
 }
 
-char *extract_line(char *buffer)
+char	*extract_line(char *buffer)
 {
-	int len = 0, i = 0;
+	char	*line;
+	int		len;
+	int		i;
 
+	len = 0;
+	i = 0;
 	while (buffer[len] && buffer[len] != '\n')
 		len++;
-
-	char *line = malloc(sizeof(char) * (len + 1 + (buffer[len] == '\n')));
+	line = malloc(sizeof(char) * (len + 1 + (buffer[len] == '\n')));
 	if (!line)
 		return (NULL);
-
 	while (i < len)
 	{
 		line[i] = buffer[i];
@@ -110,10 +106,14 @@ char *extract_line(char *buffer)
 	return (line);
 }
 
-char *refresh_buffer(char *buffer)
+char	*refresh_buffer(char *buffer)
 {
-	int i = 0, j = 0;
+	char	*new_buffer;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
 	if (!buffer[i])
@@ -121,11 +121,12 @@ char *refresh_buffer(char *buffer)
 		free(buffer);
 		return (NULL);
 	}
-
-	char *new_buffer = malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
+	new_buffer = malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
 	if (!new_buffer)
+	{
+		free(buffer);
 		return (NULL);
-
+	}
 	i++;
 	while (buffer[i])
 	{
@@ -138,18 +139,17 @@ char *refresh_buffer(char *buffer)
 	return (new_buffer);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *buffer;
+	static char	*buffer;
+	char		*line;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
 	buffer = read_and_hold(fd, buffer);
 	if (!buffer)
 		return (NULL);
-
-	char *line = extract_line(buffer);
+	line = extract_line(buffer);
 	buffer = refresh_buffer(buffer);
 	return (line);
 }
-
