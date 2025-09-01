433230b3c23e5121bece16a5ff44ba024b7cd245
Suria Narayanan Ramasamy <sramasam@u90z04s03.42kl.edu.my>
2025-09-01 21:28:56 +0800

    Bonus files Overall code is very similar to mandatory with minor changes as below - Static char *buffer require maximum number of fd can be opened   simultaneously. - Include fd for all the buffer parameter within the function.

---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sramasam <sramasam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 13:15:41 by sramasam          #+#    #+#             */
+/*   Updated: 2025/09/01 21:07:51 by sramasam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+size_t	ft_strlen(const char *s);
+int		has_newline(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+char	*ft_join_and_free(char *buffer, char *temp);
+char	*get_next_line(int fd);
+
+#endif
